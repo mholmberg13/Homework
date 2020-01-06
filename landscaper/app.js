56ef66c10610c5966ab21a294bea0fb666cf79e8
Matt Holmberg <mholmberg13@gmail.com>
2020-01-06 14:02:03 -0700

    user can use teeth to cut grass

---
+++ landscaper/app.js
@@ -0,0 +1,31 @@
+
+const tools = ["teeth", "scissors", "old mower", "fancy mower", "starving students"]
+let money = 0;
+let currentTool = tools[0];
+i = 0;
+console.log(currentTool);
+
+const upgradeToolButton = document.getElementById("upgrade");
+
+const upgrade = () => {
+    currentTool = tools[i += 1];
+    console.log(currentTool);
+    return i;
+}
+upgradeToolButton.addEventListener('click', upgrade)
+
+
+const cutGrassButton = document.getElementById("cutGrass");
+
+const cutLawn = () => {
+   
+    if (currentTool == tools[0]) {
+        money += 1;
+    
+    }
+    console.log(money);
+}
+    
+cutGrassButton.addEventListener('click', cutLawn);
+
+
