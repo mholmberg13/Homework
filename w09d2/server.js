89cc865d6c9ebaf9531ec8c1a3859665e02361d1
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-02-11 17:14:08 -0600

    Tip Calculator express application created

--- w09d2/server.js
+++ w09d2/server.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
+
+
 app.get("/greeting/:name", (req, res) => {
     res.send(`hello ${req.params.name}`);
+});
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send(`${req.params.total * req.params.tipPercentage * 0.01}`);
 });
 
 app.listen(3000, () => {
