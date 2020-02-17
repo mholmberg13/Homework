852d17f0d3b4b378ffc9230261d931b48a458e70
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-02-17 17:32:40 -0600

    table added to index.ejs

--- w10d2/Budgtr/server.js
+++ w10d2/Budgtr/server.js
@@ -4,7 +4,13 @@ const port = 3000;
 
 const budget = require("./models/budget.js");
 
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
+app.get("/", (req, res) => {
+    res.render("index.ejs", {budget: budget});
+})
 
 
 
