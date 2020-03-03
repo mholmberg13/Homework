1f37fd5bbaa5389f77fd4d328eb05b57b6a6a86f
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-03-02 21:58:35 -0600

    finished seed route and added the router

---
+++ w11d3/mongoose_store/controllers/product_router.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const router = express.Router()
+const Products = require("../models/products.js");
+
+////////////////////////////////
+
+// NEW
+router.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+// EDIT
+router.get('/:id/edit', (req, res) => {
+    Products.findById(req.params.id, (err, newProduct) => {
+        res.render('edit.ejs', {
+            product: newProduct
+        });
+    });
+});
+
+// SHOW
+router.get('/:id', (req, res) => {
+    Products.findById(req.params.id, (err, newProduct) => {
+        res.render("show.ejs", {
+            product: newProduct
+        });
+    });
+});
+
+// SEED
+router.get("/seed", async (req, res) => {
+    const newProducts =
+    [
+      {
+        name: 'Beans',
+        description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+        img: 'https://cdn3.bigcommerce.com/s-a6pgxdjc7w/products/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2',
+        price: 5,
+        qty: 99
+      }, {
+        name: 'Bones',
+        description: 'It\'s just a bag of bones.',
+        img: 'http://bluelips.com/prod_images_large/bones1.jpg',
+        price: 25,
+        qty: 0
+      }, {
+        name: 'Bins',
+        description: 'A stack of colorful bins for your beans and bones.',
+        img: 'http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg',
+        price: 7000,
+        qty: 1
+      }
+    ]
+
+  try {
+    const seedItems = await Products.create(newProducts)
+    res.send(seedItems)
+  } catch (err) {
+    res.send(err.message)
+  }
+})
+
+// INDEX
+router.get("/", (req, res) => {
+    Products.find({}, (error, products) => {
+        res.render("index.ejs", { products });
+    });
+    
+});
+
+
+//////////////////////////////////
+
+// CREATE
+router.post('/', (req, res) => {
+    Products.create(req.body, (err, data) => {
+        res.redirect('/index/')
+    });
+});
+
+// DESTROY
+router.delete('/:id', (req, res) => {
+    Products.findByIdAndDelete(req.params.id, (err, data) => {
+        res.redirect('/index/');
+    });
+});
+
+// UPDATE
+router.put('/:id', (req, res) => {
+    Products.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, data) => {
+        res.redirect('/index');
+    });
+});
+
+// BUY
+router.put("/:id/buy", (req, res) => {
+    Products.findById(req.params.id, (err, boughtProduct) => {
+        boughtProduct.qty -= 1;
+        Products.findByIdAndUpdate(req.params.id, boughtProduct, (err, data) => {
+          res.redirect(`/index/${req.params.id}`);
+        });
+    });
+});
+
+
+
+module.exports = router
--- w11d3/mongoose_store/server.js
+++ w11d3/mongoose_store/server.js
@@ -18,72 +18,10 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongo");
 });
 
-const Products = require("./models/products.js");
-
-// NEW
-app.get('/index/new', (req, res) => {
-    res.render('new.ejs');
-});
-
-// EDIT
-app.get('/index/:id/edit', (req, res) => {
-    Products.findById(req.params.id, (err, newProduct) => {
-        res.render('edit.ejs', {
-            product: newProduct
-        });
-    });
-});
-
-// SHOW
-app.get('/index/:id', (req, res) => {
-    Products.findById(req.params.id, (err, newProduct) => {
-        res.render("show.ejs", {
-            product: newProduct
-        });
-    });
-});
-
-// INDEX
-app.get("/index", (req, res) => {
-    Products.find({}, (error, products) => {
-        res.render("index.ejs", { products });
-    });
-    
-});
-
-//////////////////////////////////
-
-// CREATE
-app.post('/index', (req, res) => {
-    Products.create(req.body, (err, data) => {
-        res.redirect('/index/')
-    });
-});
-
-// DESTROY
-app.delete('/index/:id', (req, res) => {
-    Products.findByIdAndDelete(req.params.id, (err, data) => {
-        res.redirect('/index/');
-    });
-});
-
-// UPDATE
-app.put('/index/:id', (req, res) => {
-    Products.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, data) => {
-        res.redirect('/index');
-    });
-});
-
-// BUY
-app.put("/index/:id/buy", (req, res) => {
-    Products.findById(req.params.id, (err, boughtProduct) => {
-        boughtProduct.qty -= 1;
-        Products.findByIdAndUpdate(req.params.id, boughtProduct, (err, data) => {
-          res.redirect(`/index/${req.params.id}`);
-        });
-    });
-});
-
+const productController = require("./controllers/product_router.js");
+// any routes that come in for fruits should be sent
+// to the fruitsContoller
+app.use("/index", productController);
 
 // Web server:
 app.listen(3001, () => {
