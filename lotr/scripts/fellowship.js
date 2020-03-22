e95228955b21854ca590849537e7c86b328bb0bd
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-03-21 21:35:14 -0500

    Chapter 7 complete - Strider changed to Aragorn

--- lotr/scripts/fellowship.js
+++ lotr/scripts/fellowship.js
@@ -173,7 +173,10 @@ const beautifulStranger = () => {
 
   // 1. change the buddy 'Strider' textnode to "Aragorn"
      // hint: You can get a list of elements by tag name, such as 'aside'
-
+  buddies.splice(3, 1, "Aragorn")
+  console.log(buddies);
+  $($buddies).remove();
+  makeBuddies();
 };
 
 // COMMIT YOUR WORK
