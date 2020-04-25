035530b2a3e0dcb141c71ffde6d5d4bc364f534e
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-04-25 15:28:40 -0500

    Commit 5: added routes and controller

---
+++ w20d2/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -0,0 +1,12 @@
+class TweetsController < ApplicationController
+
+    def index
+        tweets = Tweet.all
+        render json: {status: 200, tweets: tweets}
+    end
+    def show
+        tweet = Tweet.find(params[:id])
+        render json: {status: 200, tweet: tweet}
+    end
+
+end
