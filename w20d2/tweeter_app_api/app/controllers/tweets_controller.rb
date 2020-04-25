8d7bf3557c3eceddc4f27c9748df7257884d424f
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-04-25 15:45:25 -0500

    Commit 8: Can POST new tweets

--- w20d2/tweeter_app_api/app/controllers/tweets_controller.rb
+++ w20d2/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -4,9 +4,26 @@ class TweetsController < ApplicationController
         tweets = Tweet.all
         render json: {status: 200, tweets: tweets}
     end
+
     def show
         tweet = Tweet.find(params[:id])
         render json: {status: 200, tweet: tweet}
+    end
+
+    def create
+        tweet = Tweet.new(tweet_params)
+    
+        if tweet.save
+          render json: { tweet: tweet }
+        else
+          render(status: 422, json: { tweet: tweet, errors: tweet.errors })
+        end
+      end
+
+    private
+
+    def tweet_params
+        params.required(:tweet).permit(:title, :content, :author)
     end
 
 end
