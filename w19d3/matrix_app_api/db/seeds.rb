dc610fbbc907f19791d5f07dd5e36224542cd27c
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-04-27 15:47:50 -0500

    Reset database

--- w19d3/matrix_app_api/db/seeds.rb
+++ w19d3/matrix_app_api/db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "Neo (born as Thomas A. Anderson, also known as The One, an anagram for Neo) is a fictional character and the protagonist of the Matrix franchise. He was portrayed as a cybercriminal and computer programmer by Keanu Reeves in The Matrix Trilogy, as well as having a cameo in The Animatrix short film Kid's Story. Andrew Bowen provided Neo's voice in The Matrix: Path of Neo. In 2008, Neo was selected by Empire as the 68th Greatest Movie Character of All Time.[1] Neo is also an anagram of "one", a reference to his destiny of being The One who would bring peace.[2] There are claims that a nightclub in Chicago inspired the name of the character in the Matrix" },
+	{ name: "Trinity", description: "Morpheus is the captain of the Nebuchadnezzar, which is a hovercraft of the human forces of the last human city, Zion, in a devastated world where most humans are grown by sentient machines and kept imprisoned in the Matrix, a virtual computer-generated world. Morpheus was once a human living inside the Matrix until he was freed." },
+	{ name: "Morpheus", description: "Trinity is a fictional character in the Matrix franchise. She is portrayed by Carrie-Anne Moss in the films. In the gameplay segments of Path of Neo, she is voiced by Jennifer Hale. Trinity first appears in the first film in the trilogy, The Matrix." },
+	{ name: "Agent Smith", description: "Agent Smith is a fictional character and the main antagonist of The Matrix trilogy. He is portrayed by Hugo Weaving in the films and voiced by Christopher Corey Smith in The Matrix: Path of Neo." },
 ])
