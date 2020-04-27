8b01e2075c1ad91ffa01f3d6b8f31bf238f11229
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-04-27 17:17:38 -0500

    Commit 11: Relations

--- w19d3/matrix_app_api/db/seeds.rb
+++ w19d3/matrix_app_api/db/seeds.rb
@@ -6,8 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: "ablaldf" },
-	{ name: "Trinity", description: "Morpheus is the captain of the Nebuchadnezzar, which is a hovercraft of the human forces of the last human city, Zion, in a devastated world where most humans are grown by sentient machines and kept imprisoned in the Matrix, a virtual computer-generated world. Morpheus was once a human living inside the Matrix until he was freed." },
-	{ name: "Morpheus", description: "Trinity is a fictional character in the Matrix franchise. She is portrayed by Carrie-Anne Moss in the films. In the gameplay segments of Path of Neo, she is voiced by Jennifer Hale. Trinity first appears in the first film in the trilogy, The Matrix." },
-	{ name: "Agent Smith", description: "Agent Smith is a fictional character and the main antagonist of The Matrix trilogy. He is portrayed by Hugo Weaving in the films and voiced by Christopher Corey Smith in The Matrix: Path of Neo." },
+	{ name: "Neo", vehicle_id: , description: "ablaldf" },
+	{ name: "Trinity", vehicle_id: 1 , description: "Morpheus is the captain of the Nebuchadnezzar, which is a hovercraft of the human forces of the last human city, Zion, in a devastated world where most humans are grown by sentient machines and kept imprisoned in the Matrix, a virtual computer-generated world. Morpheus was once a human living inside the Matrix until he was freed." },
+	{ name: "Morpheus", vehicle_id: 1 , description: "Trinity is a fictional character in the Matrix franchise. She is portrayed by Carrie-Anne Moss in the films. In the gameplay segments of Path of Neo, she is voiced by Jennifer Hale. Trinity first appears in the first film in the trilogy, The Matrix." },
+	{ name: "Agent Smith", vehicle_id: , description: "Agent Smith is a fictional character and the main antagonist of The Matrix trilogy. He is portrayed by Hugo Weaving in the films and voiced by Christopher Corey Smith in The Matrix: Path of Neo." },
+])
+
+Vehicle.create([
+    {name: "Nebuchadnezzar", style: "Spaceship"}
 ])
